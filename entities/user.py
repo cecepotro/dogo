fe29577fbc85ea0e2440f65a6e16c6184f5c52a1
jcecena <jantonio.cecena@gmail.com>
2026-03-20 10:46:10 -0700

    Se agregó Flas Login para la administración de la sesión

--- entities/user.py
+++ entities/user.py
@@ -1,8 +1,9 @@
 from persistence.db import get_connection
 from werkzeug.security import generate_password_hash, check_password_hash
 import pymysql
+from flask_login import UserMixin
 
-class User:
+class User (UserMixin):
     def __init__(self, id: int, name:str, email:str, password:str):
         self.id= id
         self.name = name
@@ -85,3 +86,29 @@ class User:
         except Exception as ex:
             print(f"Error login user:{ex}")
             return False
+        
+    def get_by_id(id):
+            try:
+                connection = get_connection()
+                cursor = connection.cursor(pymysql.cursors.DictCursor)
+                
+                sql = "SELECT id, name, email, password FROM user WHERE id = %s"
+                cursor.execute(sql, (id,))
+
+                user = cursor.fetchone()
+                
+                cursor.close()
+                connection.close()
+
+                if user:
+                    return User(
+                        user["id"],
+                        user["name"],
+                        user["email"],
+                        user["password"]
+                    )
+
+                return None
+            except Exception as ex:
+                print(f"Error login user:{ex}")
+                return False
